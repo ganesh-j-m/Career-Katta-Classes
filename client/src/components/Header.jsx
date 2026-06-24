6b5c905f85604293658cb2728c3b7373e86adc6d
ganesh-j-m <ganeshmore1552@gmail.com>
2026-06-24 16:35:18 +0530

    update header name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                         <FaVideo className='w-6 h-6 text-white'/>
                         </div>
                         <h1 className='text-xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent'>
-                             Career Katta Classes
+                             MITSC
                         </h1>
                         </Link>
 
